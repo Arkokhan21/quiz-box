2c44be349c24fdba3a204b30f4f72010b6a73755
Arkokhan21 <khanarko2021@gmail.com>
2022-10-10 21:54:27 +0600

    add tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Home/Home';
+import Main from './Layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      Path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>,
+        },
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
